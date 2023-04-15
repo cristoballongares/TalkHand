c5d27e35087e4b00f75cf378b1acbc0dd0fd60a9
crist138 <cristoballongares@gmail.com>
2023-04-14 22:47:20 -0600

    Mas palabras

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ with open('my_model.pkl', 'rb') as f:
     clf = pickle.load(f)
 
 # Crear un diccionario para mapear las etiquetas a las letras
-labels_dict = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H'}
+labels_dict = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I', 9: 'J', 10: 'K', 11: 'L', 12: 'M', 13: 'N'}
 
 # Inicializar la detección de manos de Mediapipe
 mp_hands = mp.solutions.hands
